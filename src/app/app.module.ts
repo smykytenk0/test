00d678793ef99787be3a1ad3b343aeff0e7db47d
smykytenk0 <mykytenk00s@gmail.com>
2020-10-14 11:34:04 +0300

    some changes in previous component, statscomponent added and hardcoded, and home components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatsComponent } from './stats-component/stats.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const appRoutes: Routes = [
+  {path:'stats', component: StatsComponent}
+  {path:'homepage', component: HomepageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatsComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
